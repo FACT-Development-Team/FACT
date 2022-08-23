1b51e8e235478962600ba0fd69ad91b8d36b4ef9
Ard Kastrati <akastrati@fb.com>
2022-08-22 20:05:44 -0700

    Adding argument and parser for FACTLIB to make it easier to use

--- FACTLIB/main.py
+++ FACTLIB/main.py
@@ -9,12 +9,38 @@ from synthetic.finite import Finite
 
 from generate_from_class import generate_from_class
 from generate_from_class import ClassType
+import click
 
-"""
-A simple example how to generate a sequence with length 3 from every class type.
-"""
-for class_type in ClassType:
-    expression = generate_from_class(class_type, length=3)
-    print(class_type)
-    print(expression.to_string())
-    print([expression.evaluate(i) for i in range(10)])
+@click.group()
+def cli():
+    pass
+
+########################################
+# Run
+@cli.command()
+@click.option('-s', '--sequence', type=click.Choice(['polynomial', 'trigonometric', 'exponential', "prime", "modulo", "periodic","finite"]), required=False)
+@click.option('-l', '--length', help="Length of the expression that you want to generate.", required=False)
+def generate(sequence, length):
+    """A simple example how to generate a sequences with FACTLIB. Supported class_type are
+    polynomial, trigonometric, exponential, prime, modulo, periodic, finite. Note that classes like unique,
+    bounded and increasing are not generated with context free grammars, but are meta-categories.
+    """
+    if sequence:
+        sequence = ClassType[sequence.upper()]
+        print(sequence)
+        if length:
+            expression = generate_from_class(sequence, length=int(length))
+        else:
+            expression = generate_from_class(sequence)
+        print("Expression: ", expression.to_string())
+        print("First 10 values: ", [expression.evaluate(i) for i in range(10)])
+    else:
+        for class_type in ClassType:
+            print(class_type)
+            expression = generate_from_class(class_type, length=3)
+            print(expression.to_string())
+            print([expression.evaluate(i) for i in range(10)])
+
+
+if __name__ == '__main__':
+    cli()
